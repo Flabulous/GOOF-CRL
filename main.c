60361b37bf43a0aa378ebb4e35d1b4e4fca49467
Arandor <33958575+Flabulous@users.noreply.github.com>
2020-11-25 13:53:30 -0500

    added entity

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 struct entity {
     char typ; //Type of Entity (Player, Monster, Chest, Wall)
     char hit; //Hitpoints (Used to determine Status)
     char atk; //Attack (Used for combat)
     char def; //Defense (Used for combat)
     char sts; //Status (Alive, Dead, or Static)
-    char inv[]; //Inventory (Armor, Weapon, Potion, Gold)
+    char inv[6]; //Inventory (Armor, Weapon, Potion, Gold, X Position, Y Position)
 };
 
-int
+
+
+struct entity ldr[7][7]; //Loaded Room
+
+int gen() //Generate a dungeon
+{
+
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("GOOF-CRL Loaded.\n");
     return 0;
 }
